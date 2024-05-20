9f1aaa57b9fa98d6bd0af4d0181d6d8b537b4b5e
Dimasyudistira20 <161911612+Dimasyudistira20@users.noreply.github.com>
2024-05-20 21:41:39 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -45,4 +45,52 @@ Route::get('/about2/{nama}/{jk}/{alamat}', function (Request $request , $nama , 
     $alamat2 = $alamat;
 
     return view('bio',compact('nama2','jk2','alamat2'));
+     
+});
+
+Route::get('relasi-1', function(){
+    $mahasiswa=App\Models\Mahasiswa::where('nim','=','1015015072')->first();
+
+    return $mahasiswa->wali->nama;
+});
+
+Route::get('relasi-2', function(){
+    $mahasiswa=App\Models\Mahasiswa::where('nim','=','1015015072')->first();
+
+    return $mahasiswa->dosen->nama;
+});
+
+Route::get('relasi-3',function(){
+    $dosen = App\Models\Dosen::where('nama','=','Yulianto')->first();
+
+    foreach ($dosen->mahasiswa as $data){
+        echo "<li>Nama : <strong>".$data->nama."-"."</strong>".$data->nim."</li>";
+    }
+});
+
+Route::get('relasi-4',function(){
+   $novay = App\Models\Mahasiswa::where('nama','=','Noviyanto Rachmadi')->first();
+
+   foreach($novay->hobi as $data){
+    echo '<li>'.$data->hobi.'</li>';
+   }
+});
+
+Route::get('relasi-5', function () {
+    # Temukan hobi Mandi Hujan
+    $mandi_hujan = App\Models\Hobi::where('hobi', '=', 'Mandi Hujan')->first();
+
+    # Tampilkan semua mahasiswa yang punya hobi mandi hujan
+    foreach ($mandi_hujan->mahasiswa as $temp) {
+        echo '<li> Nama : ' . $temp->nama . ' <strong>' . $temp->nim . '</strong></li>';
+    }
+
+});
+
+Route::get('eloquent', function () {
+    # Ambil semua data mahasiswa (lengkap dengan semua relasi yang ada)
+    $mahasiswa = App\Models\Mahasiswa::with('wali', 'dosen', 'hobi')->get();
+
+    # Kirim variabel ke View
+    return view('eloque', compact('mahasiswa'));
 });
